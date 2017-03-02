1d2c3686be8ce09bb44d075c8da5dc22adae3392
AlexLiang <liangalex91@gmail.com>
2017-03-02 14:36:04 -0500

    Add background to Game

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
 module.exports = {
-  entry: ['./js/main.js'],
+  entry: ['./lib/game.js'],
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'js/'),
     filename: 'bundle.js',
     publicPath: 'js/'
   },
-  modules: {
-    loader: "css-loader",
-    
+  devtool: 'source-maps',
+  resolve: {
+   extensions: [".js"]
   }
-  devtool: 'source-maps'
 };
